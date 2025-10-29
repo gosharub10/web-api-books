de666109b4425ac12765d78697e18280650e60fd
Maxim Bogdanovich <bogdanovicha97@bk.ru>
2025-10-29 14:33:32 +0300

    udp: program

--- Web.DAL/Migrations/20251029113231_InitialCreate.cs
+++ Web.DAL/Migrations/20251029113231_InitialCreate.cs
@@ -60,8 +60,8 @@ namespace Web.DAL.Migrations
                 columns: new[] { "Id", "AuthorId", "PublishYear", "Title" },
                 values: new object[,]
                 {
-                    { new Guid("ba878172-ff3d-4577-94ae-aa032d8e6177"), new Guid("075bee32-2334-4d14-8867-e348ec4fb419"), 1866, "Преступление и наказание" },
-                    { new Guid("f09236b7-f69e-4cbe-9564-8c04b4f7baff"), new Guid("9d650ba5-7dce-4d35-aedd-ffe78a67b5f1"), 1869, "Война и мир" }
+                    { new Guid("219fa2c0-3676-43bd-b0c0-3b3bc89ec3a0"), new Guid("9d650ba5-7dce-4d35-aedd-ffe78a67b5f1"), 1869, "Война и мир" },
+                    { new Guid("c6db4cc8-7cd5-46f0-9c1b-0562ef54fc36"), new Guid("075bee32-2334-4d14-8867-e348ec4fb419"), 1866, "Преступление и наказание" }
                 });
 
             migrationBuilder.CreateIndex(
